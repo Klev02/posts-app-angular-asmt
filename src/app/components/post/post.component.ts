97a327fc8bdae3146baccb4a1be7c4559b6fdd67
Klev02 <kollarlevi@gmail.com>
2024-07-04 17:20:07 +0200

    rotate through post properties and set active/inactive state

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -3,7 +3,10 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnChanges,
+  OnInit,
   Output,
+  SimpleChanges,
 } from '@angular/core';
 import { Post } from '../../interface/post';
 
@@ -15,13 +18,40 @@ import { Post } from '../../interface/post';
   styleUrl: './post.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PostComponent {
-  @Input()
+export class PostComponent implements OnChanges {
+  @Input({ required: true })
   post!: Post;
-
+  @Input() isActive: boolean = false;
   @Output() clickOnPost: EventEmitter<Post> = new EventEmitter<Post>();
 
+  postContent: any;
+
+  private postProperties!: string[];
+  private currentPopertyIndex!: number;
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['post']) {
+      this.postProperties = Object.keys(this.post);
+      this.setToDefault();
+    } else if (changes['isActive'] && !this.isActive) {
+      this.setToDefault();
+    }
+  }
+
   onPostClick(): void {
+    this.showNextPostProperty();
     this.clickOnPost.emit(this.post);
+  }
+
+  private showNextPostProperty(): void {
+    this.currentPopertyIndex =
+      (this.currentPopertyIndex + 1) % this.postProperties.length;
+    this.postContent =
+      this.post[this.postProperties[this.currentPopertyIndex] as keyof Post];
+  }
+
+  private setToDefault(): void {
+    this.postContent = this.post.title;
+    this.currentPopertyIndex = 0;
   }
 }
